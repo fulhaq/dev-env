d826d5b2b4bd425ddd90ec414dc1f0e0c404d5a5
Farman Ulhaq <fulhaq@napa.com>
2020-05-12 12:34:50 -0400

    updated win10 vagrant file

--- devenvup.sh
+++ devenvup.sh
@@ -12,3 +12,5 @@ sudo add-apt-repository 'deb [arch=amd64] https://download.virtualbox.org/virtua
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 sudo apt update
 sudo apt install virtualbox-6.1
+sudo adduser $USER vboxusers
+
